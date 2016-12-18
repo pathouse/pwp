026ba9f11c689a11d532bd62d7db3ca3df798f3e
pathouse <pat.mcgee@plated.com>
2016-12-18 14:53:34 -0500

    cleanup templates, fix tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Pwp.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Pairing with Pat.com"
   end
 end
